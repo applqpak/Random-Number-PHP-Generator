6b272dd3075893c8adc07bbc0ea06e6fe74248bf
Friendship-LEET <ihasfriends4@gmail.com>
2016-05-29 18:01:49 +0400

    Update and rename rand.html to rand.php

---
+++ rand.php
@@ -0,0 +1,15 @@
+<html>
+<center>
+<h1>Random Number Generator: By Frienship ^-^</h1>
+<h3>
+<?php
+//Made By Friendship :P 
+$number = rand(1,100);   //Change "100" to the number of contestants.
+print $number;
+if ($number === 7) {          //This part isn't necessary. I added it for the lols
+print " Lucky Number 7! Congrats!";     //This line as well isn't necessary.
+	}
+?>
+</h3>
+</center>
+</html>
