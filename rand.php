e56611f17ada917e8ef5c4b8f745f10e130472dc
applqpak <applqpak@gmail.com>
2016-06-23 19:11:27 +1000

    Update rand.php

--- rand.php
+++ rand.php
@@ -1,12 +1,22 @@
-<html>
-<center>
-<h1>Random Number Generator: By Frienship ^-^</h1>
-<h3>
+<!DOCTYPE HTML>
+  <html>
+    <head>
+      <title>
+        Random Number Generator By Friendship
+      </title>
+    </head>
+    <body>
+      <center>
+        <h1>
+          Random Number Generator: By Frienship ^-^
+        </h1>
+      </center>
+    </body>
+  </html>
 <?php
-//Made By Friendship :P 
-$number = rand(1,100);   //Change "100" to the number of contestants.
-print $number . " " . "Was randomly chosen between 1 to 100";
+  /*
+  Random Number Generator By Friendship! ^~^
+  */
+  $rand = rnad(1, 100); // Change 100 to however many contestants are needed.
+  Echo "<h3>The random number is: " . $rand . "</h3>";
 ?>
-</h3>
-</center>
-</html>
